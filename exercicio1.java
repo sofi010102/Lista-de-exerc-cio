5f1262c33b1453f3f53b5ccb9a8f46029516ddde
Sofiz <sofia.romano@aluno.ifsp.edu.br>
2025-06-17 21:19:30 -0300

    Update exercicio1.java

--- exercicio1.java
+++ exercicio1.java
@@ -1,44 +1,34 @@
-function numero() {
-    n= parseFloat(document.formulario1.n.value);
-    if(n%2 ==0) {
-        alert('Seu numero e par');
-    }else{
-        alert('Seu numero e impar');
-    }
-
-    
-<br>
-
-
-}
-
+//index.html
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Verificar Número Par ou Ímpar</title>
-    <script>
-        function numero() {
-            const n = parseFloat(document.formulario1.n.value);
-
-            if (isNaN(n)) {
-                alert('Por favor, digite um número válido.');
-                return;
-            }
-
-            if (n % 2 === 0) {
-                alert('Seu número é par');
-            } else {
-                alert('Seu número é ímpar');
-            }
-        }
-    </script>
+    <script src="script.js"></script> 
 </head>
 <body>
+    <h2>Verificador de Número Par ou Ímpar</h2>
     <form name="formulario1">
         Número: <input type="text" name="n" />
-        <input type="button" value="Verificar" onclick="numero()" />
+        <input type="button" value="Verificar" onclick="verificarNumero()" />
     </form>
 </body>
 </html>
+
+//funcao.js
+unction verificarNumero() {
+    const n = parseFloat(document.formulario1.n.value);
+
+    if (isNaN(n)) {
+        alert('Por favor, digite um número válido.');
+        return;
+    }
+
+    if (n % 2 === 0) {
+        alert('Seu número é par');
+    } else {
+        alert('Seu número é ímpar');
+    }
+}
+
